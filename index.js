90388998ccd6b488a058dd73216e7eb638a173ab
ilyasgy <ilyasingy@Gmail.com>
2025-12-16 01:13:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve("public/index.html"));
+  res.sendFile(path.resolve("index.html"));
 });
 
 app.listen(process.env.PORT || 3000);
