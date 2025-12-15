92225cd072dcd203ce60196a55c71c8edd432095
ilyasgy <ilyasingy@Gmail.com>
2025-12-16 01:11:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const http = require("http");
+import express from "express";
+import path from "path";
 
 http.createServer((req, res) => {
   res.end("Threatnest is live");
 }).listen(process.env.PORT || 3000);
+
+const app = express();
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("public/index.html"));
+});
+
+app.listen(process.env.PORT || 3000);
