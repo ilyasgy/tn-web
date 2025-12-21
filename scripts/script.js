2ec01e4bb530be87049b4347706f853c566d3955
ilyasgy <ilyasingy@Gmail.com>
2025-12-21 20:48:22 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,18 @@ const id = a.getAttribute('href').slice(1);
 const el = document.getElementById(id);
 if (el) { e.preventDefault(); el.scrollIntoView({ behavior: 'smooth' }); }
 });
+
 });
+
+console.log("Config loaded:", window.__CONFIG__);
+
+const API = window.__CONFIG__.API_BASE_URL;
+
+fetch(`${API}/health`)
+  .then(res => res.json())
+  .then(data => {
+    console.log("Backend response:", data);
+  })
+  .catch(err => {
+    console.error("Backend error:", err);
+  });
